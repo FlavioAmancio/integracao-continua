4353ebeb4afe01f556cda77ab0580976fdc921fc
Flávio Amâncio <amancio.flavio@yahoo.com.br>
2026-02-06 02:35:33 -0300

    Ajustes de conexão com banco, Docker e dependências Go

--- database/db.go
+++ database/db.go
@@ -1,29 +1,33 @@
 package database
 
 import (
-	"log"
+	"fmt"
 	"os"
+
 	"github.com/guilhermeonrails/api-go-gin/models"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var (
-	DB  *gorm.DB
-	err error
-)
+var DB *gorm.DB
 
-func ConectaComBancoDeDados() {
-    endereco := os.Getenv("DB_HOST")
-    usuario := os.Getenv("DB_USER")
-    senha := os.Getenv("DB_PASSWORD")
-    nomeBanco := os.Getenv("DB_NAME")
-    portaBanco := os.Getenv("DB_PORT")
-    stringDeConexao := "host=" + endereco + " user=" + usuario + " password=" + senha + " dbname=" + nomeBanco + " port=" + portaBanco + " sslmode=disable"
-    DB, err = gorm.Open(postgres.Open(stringDeConexao))
-    if err != nil {
-        log.Panic("Erro ao conectar com banco de dados")
-    }
+func ConectaComBancoDeDados() error {
+	endereco := os.Getenv("DB_HOST")
+	usuario := os.Getenv("DB_USER")
+	senha := os.Getenv("DB_PASSWORD")
+	nomeBanco := os.Getenv("DB_NAME")
+	portaBanco := os.Getenv("DB_PORT")
 
-    _ = DB.AutoMigrate(&models.Aluno{})
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		endereco, usuario, senha, nomeBanco, portaBanco,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	DB = db
+	return DB.AutoMigrate(&models.Aluno{})
 }
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"log"
+
 	"github.com/guilhermeonrails/api-go-gin/database"
 	"github.com/guilhermeonrails/api-go-gin/routes"
 )
 
 func main() {
-	database.ConectaComBancoDeDados()
+	if err := database.ConectaComBancoDeDados(); err != nil {
+		log.Fatal(err) // encerra com log limpo
+	}
+
 	routes.HandleRequest()
 }
